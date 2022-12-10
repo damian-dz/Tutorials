247e2a45b3c362d3c43a94acd3083cd1c92b8e8b
damian-dz <damiandzm@gmail.com>
2022-12-11 00:41:23 +0100

    update:

---
+++ BlazorFromJS/wwwroot/index.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+class Index {
+    constructor(objRef, textArea) {
+        new ResizeObserver(() => {
+            objRef.invokeMethod('TextAreaResized', textArea.clientWidth, textArea.clientHeight);
+        }).observe(textArea);
+    }
+}
+
+export function createInstance(objRef, textArea) {
+    return new Index(objRef, textArea);
+}
