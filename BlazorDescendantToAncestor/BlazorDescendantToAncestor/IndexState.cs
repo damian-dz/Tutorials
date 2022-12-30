b4ea8ba941d94d065d981c8391916fd0a8dec754
damian-dz <damiandzm@gmail.com>
2022-12-30 01:06:27 +0100

    update

---
+++ BlazorDescendantToAncestor/BlazorDescendantToAncestor/IndexState.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorDescendantToAncestor;
+
+public class IndexState
+{
+    public event Action<string>? OnGrandchildClick;
+
+    public void GrandchildClicked(string text)
+    {
+        OnGrandchildClick?.Invoke(text);
+    }
+}
---
+++ BlazorDescendantToAncestor/BlazorDescendantToAncestor/Program.cs
@@ -0,0 +1,12 @@
+using BlazorDescendantToAncestor;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IndexState>();
+
+await builder.Build().RunAsync();
