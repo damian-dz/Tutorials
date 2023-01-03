884c3c23eea4e6001377ad4228d334bc01d756f0
damian-dz <damiandzm@gmail.com>
2023-01-03 02:30:54 +0100

    update

---
+++ Blazor_TreeView/Blazor_TreeView/Models/TreeViewItem.cs
@@ -0,0 +1,19 @@
+﻿namespace Blazor_TreeView.Models;
+
+public class TreeViewItem
+{
+    public string? Text { get; set; }
+    public List<TreeViewItem>? Children { get; set; }
+    public bool IsCollapsed { get; set; }
+    public bool IsSelected { get; set; }
+
+    public bool HasChildren => Children != null && Children.Any();
+
+    public TreeViewItem(string? text, List<TreeViewItem>? children = null, bool isCollapsed = false, bool isSelected = false)
+    {
+        Text = text;
+        Children = children;
+        IsCollapsed = isCollapsed;
+        IsSelected = isSelected;
+    }
+}
